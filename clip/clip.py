89e82be8dcf5b65caf968c3aa0ee41baefcec7db
Lele Chen <141955209+lelechen63-sony@users.noreply.github.com>
2023-08-21 14:51:09 -0700

    Update clip.py

--- clip/clip.py
+++ clip/clip.py
@@ -228,6 +228,8 @@ def tokenize(texts: Union[str, List[str]], context_length: int = 77, truncate: b
     sot_token = _tokenizer.encoder["<|startoftext|>"]
     eot_token = _tokenizer.encoder["<|endoftext|>"]
     all_tokens = [[sot_token] + _tokenizer.encode(text) + [eot_token] for text in texts]
+
+    print ('all_tokens', all_tokens.shape)
     if packaging.version.parse(torch.__version__) < packaging.version.parse("1.8.0"):
         result = torch.zeros(len(all_tokens), context_length, dtype=torch.long)
     else:
