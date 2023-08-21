73c907a95c371451977a286d98ce3aa8c36a4141
Lele Chen <141955209+lelechen63-sony@users.noreply.github.com>
2023-08-21 14:37:10 -0700

    Update debug.py
    debug

--- debug.py
+++ debug.py
@@ -4,13 +4,19 @@ from PIL import Image
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model, preprocess = clip.load("ViT-B/32", device=device)
+print ('preprocess', preprocess)
 
 image = preprocess(Image.open("CLIP.png")).unsqueeze(0).to(device)
+print ( 'image shape',  image.shape)
 text = clip.tokenize(["a diagram", "a dog", "a cat"]).to(device)
+print( 'text shape:', text.shape)
 
 with torch.no_grad():
     image_features = model.encode_image(image)
+    print( 'image_features shape:', image_features.shape)
+
     text_features = model.encode_text(text)
+    print( 'text_features shape:', text_features.shape)
     
     logits_per_image, logits_per_text = model(image, text)
     probs = logits_per_image.softmax(dim=-1).cpu().numpy()
